8080ebfdc4a2c3a39ed65f2faa129260fda744b4
faerumidovna <mushtariyumidovna1404@gmail.com>
2023-03-31 10:47:36 +0500

    fix

---
+++ script.js
@@ -0,0 +1,93 @@
+// ================== 1 misol =====================
+
+
+// let a = +prompt('son kiriting')-0
+// let b = +prompt('son kiriting')-0
+
+// let plus = function (a,b){
+//    return a+b;
+// }
+
+// alert(plus(a,b))
+
+
+// ================== 2 misol =====================
+
+
+
+// let a = +prompt('a sonni kiriting')
+// let b = +prompt('b sonni kiriting')
+// let c = +prompt('c sonni kiriting')
+
+// function maxMin(a,b,c){  
+//       if(a>b && b>c){
+//          return ('a katta c kichik ')
+//       } else if(a<b && b<c){
+//          return ('a kichik c katta')
+//       }else if(b>c && c>a){
+//          return('b katta a kichik')
+//       }else if (c>a && a>b){
+//          return ('c katta b kichik')
+//       }else if(b>a && a>b){
+//          return ('b katta c kichik')
+//       }else if (a>c && c>b){
+//          return ('a katta b kichik')
+//       }else if(a>b && b==c) {
+//          return ('a katta b va c teng')
+//       }else if(b>a && a==c){
+//          return ('b katta a va c teng')
+//       }else if(c>a && a==b){
+//          return ('c katta a va b teng')
+//       }else if(a<b && b==c) {
+//          return ('a kichik b va c teng')
+//       }else if(b<a && a==c){
+//          return ('b kichik a va c teng')
+//       }else if(c<a && a==b){
+//          return ('c kichik a va b teng')
+//       }else if(a==b && b==c){
+//          return('hamma raqam teng')
+//       }else {
+//          return ('qayta urinib koring')
+//       }
+// }
+
+// alert (maxMin(a,b,c))
+
+
+// ================== 3 misol =====================
+
+// let a = +prompt('N ta niqobni kiriting')
+// let b = +prompt('A ta odamni kiriting')
+
+// function personMask(a,b){
+//    if(a>b && a==b){
+//       return (a%b + ' tadan togri keladi');
+//    }else if(a==b){
+//       return (a/b + ' tadan togri keladi');
+//    }
+//     if(b>a){
+//       return ('niqob yetarli emas')
+//    }else{
+//       return('try again')
+//    }
+// }
+
+// alert(personMask(a,b))
+
+// ================== 4 misol =====================
+
+// var arr = prompt("Enter your numbers").split("")
+
+// alert(arr[0])
+
+
+// ================== 5 misol =====================
+
+// let a = +prompt('a soninini kiriting')
+// let b = +prompt('b soninini kiriting')
+
+// function numb(a,b){
+//    return(b+ " va " +a)
+// }
+
+// alert(numb(a,b))
